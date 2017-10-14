7f83dd0bd7e41cf6b4805fcadeb4abe0f8df1581
Isaac Sh <iktwo.sh@gmail.com>
2017-10-14 16:25:03 -0700

    Refactor Android app's code.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 var dash_button = require('node-dash-button');
 var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://test.mosquitto.org');
+const DASH = process.env.DASH || '40:b4:cd:e5:43:fd';
+const SERVER = process.env.SERVER || 'mqtt://test.mosquitto.org';
+
+var client = mqtt.connect(SERVER);
 
 client.on('connect', function () {
-  //Nothing to do
-  console.log('Connected');
-})
+    // Nothing to do
+    console.log('Connected');
+});
 
-var dash = dash_button("40:b4:cd:e5:43:fd", null, 5000, 'udp');
-dash.on("detected", function (dash_id){
-  client.publish('nextSongTopic','Something');
+var dash = dash_button(DASH, null, 5000, 'udp');
+dash.on("detected", function (dash_id) {
+    client.publish('nextSongTopic', 'Something');
 });
